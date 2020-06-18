d086daa7cbc04331a494e4ca9734586eeee45bf9
Surender <surenderredy5228@gmail.com>
2020-06-18 16:47:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular6-springboot-crud
+Angular 6 + Spring Boot 2 + Spring Data JPA + MySQL
+
---
+++ springboot2-jpa-crud-example/src/main/java/surender/narlakanti/springboot2/springboot2jpacrudexample/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package surender.narlakanti.springboot2.springboot2jpacrudexample.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import surender.narlakanti.springboot2.springboot2jpacrudexample.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
